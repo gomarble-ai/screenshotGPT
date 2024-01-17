91ff7e5a44cb9a492acad455dcb54981bc055703
palkush <palkushraichawla@gmail.com>
2024-01-17 15:12:19 +0530

    added docker file

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,39 +2,51 @@ const puppeteer = require("puppeteer");
 
 const scrapeLogic = async (res) => {
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.emulate(puppeteer.devices['iPhone 6']);
-    await page.goto(
-        'https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&q=%22myobvi.com%22&sort_data%5Bdirection%5D=desc&sort_data%5Bmode%5D=relevancy_monthly_grouped&search_type=keyword_exact_phrase&media_type=all',
-        { waitUntil: 'networkidle0' }
-        );
-    
-    // Function to scroll to the bottom of the page
-    async function autoScroll(page){
-        await page.evaluate(async () => {
-            await new Promise((resolve, reject) => {
-                var totalHeight = 0;
-                var distance = 100;
-                var timer = setInterval(() => {
-                    var scrollHeight = document.body.scrollHeight;
-                    window.scrollBy(0, distance);
-                    totalHeight += distance;
 
-                    if(totalHeight >= scrollHeight){
-                        clearInterval(timer);
-                        resolve();
-                    }
-                }, 100);
+    try {
+        const page = await browser.newPage();
+        await page.emulate(puppeteer.devices['iPhone 6']);
+        await page.goto(
+            'https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&q=%22myobvi.com%22&sort_data%5Bdirection%5D=desc&sort_data%5Bmode%5D=relevancy_monthly_grouped&search_type=keyword_exact_phrase&media_type=all',
+            { waitUntil: 'networkidle0' }
+            );
+        
+        // Function to scroll to the bottom of the page
+        async function autoScroll(page){
+            await page.evaluate(async () => {
+                await new Promise((resolve, reject) => {
+                    var totalHeight = 0;
+                    var distance = 100;
+                    var timer = setInterval(() => {
+                        var scrollHeight = document.body.scrollHeight;
+                        window.scrollBy(0, distance);
+                        totalHeight += distance;
+
+                        if(totalHeight >= scrollHeight){
+                            clearInterval(timer);
+                            resolve();
+                        }
+                    }, 100);
+                });
             });
-        });
-    }
+        }
 
-    // Scroll to the bottom of the page
-    await autoScroll(page);
+        // Scroll to the bottom of the page
+        await autoScroll(page);
+        
+        await page.screenshot({path: 'full.png', fullPage: true});
+
+        res.send("scraping done");
+
+    } catch (e){
+        console.error(e);
+        res.send(`something went wrong y'all : ${e}`);
+
+    } finally {
+        await browser.close();
+
+    }
     
-    await page.screenshot({path: 'full.png', fullPage: true});
-    await browser.close();
-    res.send("scraping done");
 };
 
 module.exports = {scrapeLogic};
