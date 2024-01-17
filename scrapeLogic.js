f410da3ba394fcd4d5c99d138327506e767699f9
palkush <palkushraichawla@gmail.com>
2024-01-17 16:59:00 +0530

    added new files

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -19,7 +19,7 @@ const scrapeLogic = async (res) => {
         await page.emulate(puppeteer.devices['iPhone 6']);
         await page.goto(
             'https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&q=%22myobvi.com%22&sort_data%5Bdirection%5D=desc&sort_data%5Bmode%5D=relevancy_monthly_grouped&search_type=keyword_exact_phrase&media_type=all',
-            { waitUntil: 'networkidle0' }
+            { waitUntil: 'networkidle0' , timeout:0 }
             );
         
         // Function to scroll to the bottom of the page
