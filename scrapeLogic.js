7d8a9ba5a888a3e4db213f1b290f44cbe07cc178
palkush-marble <palkush@gomarble.ai>
2024-01-18 18:20:31 +0530

    added test website gomarble.ai

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -18,7 +18,11 @@ const scrapeLogic = async (req,res) => {
         const page = await browser.newPage();
         //await page.emulate(puppeteer.devices['iPhone 6']);
         await page.goto(
-            `https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&q=%22${req}%22&sort_data%5Bdirection%5D=desc&sort_data%5Bmode%5D=relevancy_monthly_grouped&search_type=keyword_exact_phrase&media_type=all`,
+            (
+                req === "gomarble.ai"
+                ? `https://www.gomarble.ai`
+                : `https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=ALL&q=%22${req}%22&sort_data%5Bdirection%5D=desc&sort_data%5Bmode%5D=relevancy_monthly_grouped&search_type=keyword_exact_phrase&media_type=all`
+            ),            
             { waitUntil: 'networkidle0' , timeout:0 }
             );
         
