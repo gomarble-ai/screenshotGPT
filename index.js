8ba0621251f993a82689f56fe9c0e8104fdbb75e
gomarble-ai <156911525+gomarble-ai@users.noreply.github.com>
2024-01-18 09:32:26 +0000

    testin digital ocean App changes

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require("express");
-const fs = require("fs");
-const https = require("https");
 const {scrapeLogic} = require("./scrapeLogic");
 const app = express();
 
-const PORT = process.env.PORT || 4430;
+const PORT = process.env.PORT || 8080;
 
-https
-  .createServer({
-    key: fs.readFileSync(__dirname+"/client-key.pem"),
-    cert: fs.readFileSync(__dirname+"/client-cert.pem"),
-  },
-    app)
-  .listen(PORT, ()=>{
+app.listen(PORT,()=>{
     console.log(`Listening on Port ${PORT}`);
-  });
+
+});
 
 app.get("/", (req,res) => {
     res.send("Render Puppeteer server is up and running");
